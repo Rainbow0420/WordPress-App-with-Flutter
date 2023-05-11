d8925b00abd03b2c444d548a0b2e40de52a199b8
KingTiger <KingTiger@gmail.com>
2023-05-11 18:15:43 +0900

    add

---
+++ lib/config.dart
@@ -0,0 +1,10 @@
+const String TITLE = "Rainbow";
+
+/// last slash ' / ' is mandatory
+const String URL = "";
+
+const int FEATURED_CATEGORY_ID = 20;
+const String FEATURED_CATEGORY_TITLE = 'Featured';
+const isRTL = false;
+
+const String REST_URL_PREFIX = 'wp-json';
